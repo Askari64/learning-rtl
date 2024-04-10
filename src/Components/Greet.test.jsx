30c637f5cb395d8914cbaef4de6eb7bcf0619266
Askari <askarir64@gmail.com>
2024-04-10 14:40:43 +0530

    grouping with describe

--- src/Components/Greet.test.jsx
+++ src/Components/Greet.test.jsx
@@ -1,23 +1,32 @@
-import {render, screen} from '@testing-library/react'
-import Greet from './Greet'
-import React from 'react'
-test('render Greet', () => { 
-    render(<Greet/>)
-    const textElement = screen.getByText('Hello')
-    expect(textElement).toBeInTheDocument()
+import { render, screen } from "@testing-library/react";
+import Greet from "./Greet";
+import React from "react";
+
+//grouped all tests in describe block
+describe('Greet Tests',()=> {
+    test("render correctly", () => {
+        render(<Greet />);
+        const textElement = screen.getByText("Hello");
+        expect(textElement).toBeInTheDocument();
+      });
+      
+      test("render Greet Without case sensitivity", () => {
+        render(<Greet />);
+        const textElement = screen.getByText(/hello/i);
+        expect(textElement).toBeInTheDocument();
+      });
+      
+      describe('Nested Describe Block',()=> {
+        test("render Greet with prop", () => {
+            render(<Greet name={"Askari"} />);
+            const textElement = screen.getByText("Hello Askari");
+            expect(textElement).toBeInTheDocument();
+          });
+      })
+      
+      
 })
 
-test('render Greet Without case sensitivity', () => { 
-    render(<Greet/>)
-    const textElement = screen.getByText(/hello/i)
-    expect(textElement).toBeInTheDocument()
-})
-
-test('render Greet with prop', () => { 
-    render(<Greet name={'Askari'}/>)
-    const textElement = screen.getByText('Hello Askari')
-    expect(textElement).toBeInTheDocument()
-})
 
 /*This will run ONLY this test in this file
 test.only('render Greet with prop', () => { 
